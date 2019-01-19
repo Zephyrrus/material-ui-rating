41d89ea7892ada70b6f4fdab94a1786a127bfe75
Zephy <krisztian1997@gmail.com>
2019-01-19 15:37:27 +0200

    Fix imports and change some code

--- src/components/Rating/Rating.js
+++ src/components/Rating/Rating.js
@@ -1,29 +1,21 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { IconButton } from 'material-ui'
-import orange from 'material-ui/colors/orange'
-import grey from 'material-ui/colors/grey'
-import { Star, StarBorder } from 'material-ui-icons'
-import { withStyles } from 'material-ui/styles'
-import classNames from 'classnames'
-
-const noPointerEvents = {
-  pointerEvents: 'none'
-}
+import IconButton from '@material-ui/core/IconButton'
+import orange from '@material-ui/core/colors/orange'
+import grey from '@material-ui/core/colors/grey'
+import { Star, StarBorder } from '@material-ui/icons'
 
 const styles = {
-  root: {},
-  iconButton: {},
-  icon: {},
-  disabled: noPointerEvents,
-  readOnly: noPointerEvents
+  disabled: {
+    pointerEvents: 'none'
+  }
 }
 
 /**
- * Material design star rating component for oyur star application!
+ * Material design star rating component for your star application!
  * @see [Card UI controls](https://material.io/guidelines/components/cards.html#cards-actions)
  */
-class Rating extends Component {
+export default class Rating extends Component {
   constructor (props) {
     super(props)
     this.state = {
@@ -35,66 +27,46 @@ class Rating extends Component {
     const filled = i <= this.props.value
     const hovered = i <= this.state.hoverValue
 
-    const normalColor = this.props.disabled ? {} : {nativeColor: grey[300]}
-    const orangeColor = this.props.disabled ? {} : {nativeColor: orange[500]}
-
-    const iconHovered = !this.props.iconHovered
-      ? <StarBorder
-        classes={{ root: this.props.classes.icon }}
-        {...orangeColor} />
-      : this.props.iconHovered
-
-    const iconFilled = !this.props.iconFilled
-      ? <Star
-        classes={{root: this.props.classes.icon}}
-        {...orangeColor} />
-      : this.props.iconFilled
-
-    const iconNormal = !this.props.iconNormal
-      ? <StarBorder
-        className={this.props.classes.icon}
-        {...normalColor} />
-      : this.props.iconNormal
-
-    if ((hovered && !filled) || (!hovered && filled)) {
+    if (!this.props.disabled && (hovered && !filled || !hovered && filled)) {
       return this.props.iconHoveredRenderer ? this.props.iconHoveredRenderer({
         ...this.props,
         index: i
-      }) : iconHovered
+      }) : this.props.iconHovered
     } else if (filled) {
       return this.props.iconFilledRenderer ? this.props.iconFilledRenderer({
         ...this.props,
         index: i
-      }) : iconFilled
+      }) : this.props.iconFilled
     } else {
       return this.props.iconNormalRenderer ? this.props.iconNormalRenderer({
         ...this.props,
         index: i
-      }) : iconNormal
+      }) : this.props.iconNormal
     }
   }
 
   render () {
-    const { classes, max, disabled, readOnly, value, onChange } = this.props
     const rating = []
 
-    for (let i = 1; i <= max; i++) {
+    for (let i = 1; i <= this.props.max; i++) {
+      const tooltip = this.props.tooltip || this.props.tooltipRenderer ? this.props.tooltipRenderer({index: i, ...this.props}) : null
+
       rating.push(
         <IconButton
           key={i}
-          className={classNames(
-            classes.iconButton,
-            {
-              [classes.disabled]: disabled,
-              [classes.readOnly]: readOnly
-            }
-          )}
-          disabled={disabled}
+          className={this.props.className}
+          disabled={this.props.disabled}
+          iconStyle={this.props.itemIconStyle}
+          iconClassName={this.props.iconClassName}
+          style={this.props.itemStyle}
+          tooltip={tooltip}
+          tooltipPosition={this.props.tooltipPosition}
+          tooltipStyles={this.props.tooltipStyles}
           onMouseEnter={() => this.setState({hoverValue: i})}
-          onMouseLeave={() => this.setState({hoverValue: value})}
+          onMouseLeave={() => this.setState({hoverValue: this.props.value})}
           onClick={() => {
-            if (!readOnly && onChange) {
-              onChange(i)
+            if (!this.props.readOnly && this.props.onChange) {
+              this.props.onChange(i)
             }
           }}
         >
@@ -103,22 +75,35 @@ class Rating extends Component {
       )
     }
 
-    return (<div className={classes.root}>{rating}</div>)
+    return (
+      <div
+        style={this.props.disabled || this.props.readOnly ? {...styles.disabled, ...this.props.style} : this.props.style}
+      >
+        {rating}
+      </div>
+    )
   }
 }
 
 Rating.defaultProps = {
   disabled: false,
+  iconFilled: <Star color={orange[500]} />,
+  iconHovered: <StarBorder color={orange[500]} />,
+  iconNormal: <StarBorder color={grey[300]} />,
+  tooltipPosition: 'bottom-center',
   max: 5,
+  min: 0,
   readOnly: false,
   value: 0
 }
 
 Rating.propTypes = {
-  /** Useful to extend the style applied to components. See the repository README for the accepted keys. */
-  classes: PropTypes.object.isRequired,
+  /** Sets classname for IconButton component. */
+  className: PropTypes.string,
   /** Disables the rating and gray it out if set to true. */
   disabled: PropTypes.bool,
+  /** Sets classname for icon in IconButton Component. */
+  iconClassName: PropTypes.string,
   /** This is the icon to be used as an icon in value range. */
   iconFilled: PropTypes.node,
   /** Overrides filled icon renderer. */
@@ -131,14 +116,28 @@ Rating.propTypes = {
   iconNormal: PropTypes.node,
   /** Overrides normal icon renderer. */
   iconNormalRenderer: PropTypes.func,
+  /** Override the inline-icon-styles of the item elements. */
+  itemIconStyle: PropTypes.object,
+  /** Override the inline-styles of the item elements. */
+  itemStyle: PropTypes.object,
   /** The max value of the rating bar. */
   max: PropTypes.number,
+  /** THe min value of the rating bar  */
+  min: PropTypes.number.
   /** Fired when a value is clicked. */
   onChange: PropTypes.func,
   /** Don't allow input if set to true. */
   readOnly: PropTypes.bool,
+  /** Override the inline-styles of the root element. */
+  style: PropTypes.object,
+  /** Sets tooltip for icon in IconButton Component. */
+  tooltip: PropTypes.node,
+  /** Overrides tooltip renderer. */
+  tooltipRenderer: PropTypes.func,
+  /** Overrides tooltip position. */
+  tooltipPosition: PropTypes.string,
+  /** Overrides tooltip styles. */
+  tooltipStyles: PropTypes.object,
   /** The value of the rating bar. */
   value: PropTypes.number
 }
-
-export default withStyles(styles)(Rating)
